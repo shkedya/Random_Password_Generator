306d7987142767ce338bcd1220600e7081ecd499
Alon <shkedya@oregonstate.edu>
2021-11-09 00:07:06 -0800

    update - added the get request from the username page

--- random_password.py
+++ random_password.py
@@ -6,6 +6,9 @@ import random
 import string
 import requests
 
+get_url = requests.get(" https://malliuxservice.herokuapp.com/username")
+display_name = get_url.text
+
 length_password = int(input("Enter the number of digits you want your password to have. "))
 capital_chars = input("Do you want your password to have capitals? Enter y or yes for yes and n or no for no. ")
 special_chars = input("Do you want your password to have special characters? e.g. @ or ! Enter y or yes for yes "
@@ -21,46 +24,46 @@ if capital_chars == "y" and special_chars == "n" and numbers == "n":
     temp_var = characters + capitals
     new_choice = random.sample(temp_var, length_password)
     password = "".join(new_choice)
-    print(password)
+    print("Hi", display_name, "! your password is", password)
 elif capital_chars == "n" and special_chars == "y" and numbers == "n":
     temp_var = characters + specials
     new_choice = random.sample(temp_var, length_password)
     password = "".join(new_choice)
-    print(password)
+    print("Hi", display_name, "! your password is", password)
 elif capital_chars == "n" and special_chars == "n" and numbers == "y":
     temp_var = characters + list_nums
     new_choice = random.sample(temp_var, length_password)
     password = "".join(new_choice)
-    print(password)
+    print("Hi", display_name, "! your password is", password)
 elif capital_chars == "y" and special_chars == "y" and numbers == "n":
     temp_var = characters + capitals + specials
     new_choice = random.sample(temp_var, length_password)
     password = "".join(new_choice)
-    print(password)
+    print("Hi", display_name, "! your password is", password)
 elif capital_chars == "y" and special_chars == "n" and numbers == "y":
     temp_var = characters + capitals + list_nums
     new_choice = random.sample(temp_var, length_password)
     password = "".join(new_choice)
-    print(password)
+    print("Hi", display_name, "! your password is", password)
 elif capital_chars == "n" and special_chars == "y" and numbers == "y":
     temp_var = characters + specials + list_nums
     new_choice = random.sample(temp_var, length_password)
     password = "".join(new_choice)
-    print(password)
+    print("Hi", display_name, "! your password is", password)
 elif capital_chars == "y" and special_chars == "y" and numbers == "y":
     temp_var = characters + capitals + specials + list_nums
     new_choice = random.sample(temp_var, length_password)
     password = "".join(new_choice)
-    print(password)
+    print("Hi", display_name, "! your password is", password)
 elif capital_chars == "n" and special_chars == "n" and numbers == "n":
     new_choice = random.sample(characters, length_password)
     password = "".join(new_choice)
-    print(password)
+    print("Hi", display_name, "! your password is", password)
 else:
     print("Sorry you did not type y or n for one or more questions, automatically printing a strong password: ")
     temp_var = characters + capitals + specials + list_nums
     new_choice = random.sample(temp_var, length_password)
     password = "".join(new_choice)
-    print(password)
+    print("Hi", display_name, "! your password is", password)
 
 # from tkinter import *  ----- will add this later (maybe probably)
