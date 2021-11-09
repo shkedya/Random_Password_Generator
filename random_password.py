52a765512a9a8c53917acadc372655aeddfe80b2
Alon <shkedya@oregonstate.edu>
2021-11-08 23:05:26 -0800

    Initial commit

---
+++ random_password.py
@@ -0,0 +1,34 @@
+# Author: Alon Shkedy
+# Last Date Modified: 11/8/2021
+# Description: A user is given a random password
+
+import random
+import string
+import requests
+
+length_password = int(input("Enter the number of characters/digits you want your password to have. "))
+capital_chars = input("Do you want your password to have capitals? Enter y or yes for yes and n or no for no. ")
+special_chars = input("Do you want your password to have special characters? e.g. @ or ! Enter y or yes for yes and n "
+                      "or no for no. ")
+numbers = input("Do you want your password to have numbers? Enter y or yes for yes and n or no for no. ")
+
+characters = string.ascii_lowercase
+capitals = string.ascii_uppercase
+specials = string.punctuation
+list_nums = string.digits
+
+if capital_chars == "y" or "yes" and special_chars == "n" or "no" and numbers == "n" or "no":
+    temp_var = characters + capitals
+    new_choice = random.sample(temp_var, length_password)
+    password = "".join(new_choice)
+    print(password)
+if capital_chars == "n" or "no" and special_chars == "y" or "yes" and numbers == "n" or "no":
+    temp_var = characters + specials
+    new_choice = random.sample(temp_var, length_password)
+    password = "".join(new_choice)
+    print(password)
+if capital_chars == "n" or "no" and special_chars == "n" or "no" and numbers == "y" or "yes":
+    temp_var = characters + list_nums
+    new_choice = random.sample(temp_var, length_password)
+    password = "".join(new_choice)
+    print(password)
