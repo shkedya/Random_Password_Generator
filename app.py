d107b5f73fc068b82e835f8e95df758d1120859b
shkedya <71806271+shkedya@users.noreply.github.com>
2024-11-24 17:01:17 -0800

    Update app.py

--- app.py
+++ app.py
@@ -1,5 +1,4 @@
 # Author: Alon Shkedy
-# Last Date Modified: 11/8/2021
 # Description: A get request is initiated to get a username, that user is given a random password based on
 #              criteria that they choose from a gui
 
